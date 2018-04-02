c7e45a588de82ad1cc37bc6c1c51e2b574abfde9
fabascal <fabascal@live.com.mx>
2018-04-02 14:16:42 -0500

    first commit

---
+++ hw/jinja2/app.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	titulo="Home!"
+	lista = ["footer","header","body"]
+	return render_template("index.html",titulo=titulo,lista=lista)
+
+if __name__=="__main__":
+	app.run(debug=True,port =9999)
+
